8c0fee3f3cda897137580c86a9a78aa1b06f95b3
mursalin@gmail.com <MURSA>
2023-09-09 20:55:31 +0600

    imported all necessary components in the main page.js / home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,25 @@
-
+import About from "./components/About";
+import Cars from "./components/Cars";
+import Cta from "./components/Cta";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Testimonial from "./components/Testimonial";
+import TopButton from "./components/TopButton";
+import WhyUs from "./components/WhyUs";
 
 export default function Home() {
   return (
     <main className="">
-      main
+      <Header />
+      <Hero />
+      <Cars />
+      <About />
+      <WhyUs />
+      <Testimonial />
+      <Cta />
+      <Footer />
+      <TopButton />
     </main>
-  )
+  );
 }
