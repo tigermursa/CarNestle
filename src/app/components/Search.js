31518d78c77603df593b8fbaf2130f827463fc46
mursalin@gmail.com <MURSA>
2023-09-11 11:52:26 +0600

    working on the hours selector page

--- src/app/components/Search.js
+++ src/app/components/Search.js
@@ -3,6 +3,7 @@ import React, { useContext } from "react";
 import { SearchContext } from "../context/search";
 import LocationSelection from "./LocationSelection";
 import DateSelection from "./DateSelection";
+import HoursSelection from "./HoursSelection";
 
 const Search = () => {
   const { searchActive } = useContext(SearchContext);
@@ -17,6 +18,7 @@ const Search = () => {
       <div className="xl:h-full flex items-center px-6 xl:px-0">
         <LocationSelection />
         <DateSelection />
+        <HoursSelection />
       </div>
     </div>
   );
