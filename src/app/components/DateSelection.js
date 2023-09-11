3821fc41cb1e6afb0482fdbb052866ba02d6b04d
mursalin@gmail.com <MURSA>
2023-09-11 09:35:08 +0600

    updated date selection

--- src/app/components/DateSelection.js
+++ src/app/components/DateSelection.js
@@ -26,6 +26,19 @@ const DateSelection = () => {
             <FaCalendarAlt className="text-accent" />
             <div className="text-[15px] uppercase font-bold ">Select Date</div>
           </div>
+          <div className="flex items-center gap-x-3 xl:ml-6 ">
+            <div className=" text-[13px] font-medium text-secondary">
+              {format(date[0].startDate, "dd.MM.yyy")}
+            </div>
+            <FaArrowRightLong className="text-accent text-[12px]" />
+            <div className=" text-[13px] font-medium text-secondary">
+              {date[0].endDate ? (
+                <div>{format(date[0].endDate, "dd/MM/yyyy")}</div>
+              ) : (
+                <div>{format(date[0].startDate, "dd/MM/yyyy")}</div>
+              )}
+            </div>
+          </div>
         </Menu.Button>
         {/* menu */}
         <Menu.Items className=" dropdown-menu shadow-lg absolute -top-96 xl:top-[90px] left-1/2 xl:left-0 z-50 transform -translate-x-1/2 xl:-translate-x-0  rounded-[10px] overflow-hidden ">
