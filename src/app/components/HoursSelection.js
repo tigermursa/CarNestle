ec0a7eb031b0e8e478b836d62513b4848ca670b3
mursalin@gmail.com <MURSA>
2023-09-11 12:56:39 +0600

    search bar ready now

--- src/app/components/HoursSelection.js
+++ src/app/components/HoursSelection.js
@@ -31,9 +31,10 @@ const HoursSelection = () => {
         </Menu.Button>
         {/* items */}
         <Menu.Items className=" dropdown-menu shadow-lg absolute -top-72 xl:top-[90px] left-1/2 xl:left-0 z-10 transform -translate-x-1/2 xl:-translate-x-0 text-sm w-full bg-white max-w-[332px] py-6 rounded-[10px] ">
-          {hours.map((hours, index) => {
+          {hours.map((hour, index) => {
             return (
               <div
+                onClick={() => setHour(hour)}
                 className="cursor-pointer py-4 text-center hover:bg-gray-50"
                 key={index}
               >
--- src/app/components/Search.js
+++ src/app/components/Search.js
@@ -15,10 +15,22 @@ const Search = () => {
           : "bg-white rounded-[20px] py-6 xl:pr-4  xl:h-[98px]"
       } hidden xl:block w-full relative shadow-lg`}
     >
-      <div className="xl:h-full flex items-center px-6 xl:px-0">
+      <div className={`flex h-full ${searchActive && "container mx-auto"} `}>
         <LocationSelection />
         <DateSelection />
         <HoursSelection />
+        {/* button */}
+        <div className="xl:h-full flex items-center px-6 xl:px-0">
+          <button
+            className={`${
+              searchActive
+                ? "btn btn-sm btn-accent xl:w-[164px]"
+                : "btn btn-lg btn-accent xl:w-[184px]"
+            }`}
+          >
+            Search
+          </button>
+        </div>
       </div>
     </div>
   );
