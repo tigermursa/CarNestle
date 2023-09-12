b4fa6352ae6f4ed337b45eb6bbe351f0829fd1de
mursalin@gmail.com <MURSA>
2023-09-12 06:34:06 +0600

    working with the why us section

--- src/app/components/WhyUs.js
+++ src/app/components/WhyUs.js
@@ -3,7 +3,7 @@ import React from "react";
 const WhyUs = () => {
   return (
     <section className="section flex items-center bg-green-200" id="why">
-      Why us ?
+      
     </section>
   );
 };
